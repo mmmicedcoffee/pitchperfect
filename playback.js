65475da55e3766e6b785e9352ea498af0bb0abe4
Jiashan Liang <susanliang3@gmail.com>
2013-05-03 05:01:41 -0400

    moved some shit around

--- playback.js
+++ playback.js
@@ -1,3 +1,60 @@
+      function timecode(ms) {
+        var hms = {
+          h: Math.floor(ms/(60*60*1000)),
+          m: Math.floor((ms/60000) % 60),
+          s: Math.floor((ms/1000) % 60)
+        };
+        var tc = []; // Timecode array to be joined with '.'
+
+        if (hms.h > 0) {
+          tc.push(hms.h);
+        }
+
+        tc.push((hms.m < 10 && hms.h > 0 ? "0" + hms.m : hms.m));
+        tc.push((hms.s < 10  ? "0" + hms.s : hms.s));
+
+        return tc.join(':');
+      }
+    
+    
+      Recorder.initialize({
+        swfSrc: "external_js/recorder.swf"
+      });
+
+      function record(){
+        Recorder.record({
+          start: function(){
+            //alert("recording starts now. press stop when youre done. and then play or upload if you want.");
+          },
+          progress: function(milliseconds){
+            document.getElementById("time").innerHTML = timecode(milliseconds);
+          }
+        });
+      }
+      
+      function play(){
+        Recorder.stop();
+        Recorder.play({
+          progress: function(milliseconds){
+            document.getElementById("time").innerHTML = timecode(milliseconds);
+          }
+        });
+      }
+      
+      function stop(){
+        Recorder.stop();
+      }
+      
+      function upload(){
+        Recorder.upload({
+          url:        "https://example.com/upload",
+          audioParam: "your_file",
+          success: function(){
+            alert("your file was uploaded!");
+          }
+        });
+      }
+
 function playExample() {
     var Synth = function(audiolet, frequency, length) {
         AudioletGroup.apply(this, [audiolet, 0, 1]);
